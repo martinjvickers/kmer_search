18abf0b1d145b7114f02fae38def38cbbf047ebb
Martin Vickers <martinj.vickers@gmail.com>
2024-04-02 14:38:13 +0100

    Testing

--- kmer_pa_matrix_new.cpp
+++ kmer_pa_matrix_new.cpp
@@ -22,13 +22,14 @@ struct ModifyStringOptions
 {
 	CharString kmerDatabasesFilenames;
 	CharString masterKmerDatabase;
+	int specific_kmer;
+	CharString outputFilename;
 };
 
 struct kmer
 {
 	uint64 k;
-	uint64 bits;
-	boost::dynamic_bitset<> x;
+	vector<uint64> bits;
 };
 
 seqan2::ArgumentParser::ParseResult parseCommandLine(ModifyStringOptions & options, 
@@ -43,10 +44,17 @@ seqan2::ArgumentParser::ParseResult parseCommandLine(ModifyStringOptions & optio
 			           "A master list of the union of all kmer databases",
                                    ArgParseArgument::STRING, "TEXT", true));
   setRequired(parser, "master-database");
+  addOption(parser, ArgParseOption("n", "specific-kmer", 
+			           "If you wish to just run PA on a specific kmer database, give that number here.",
+			           ArgParseArgument::INTEGER, "INT"));
+  addOption(parser, ArgParseOption("o", "output-file",
+			           "This is the PA matrix output file",
+				   ArgParseArgument::STRING, "TEXT", true));
+  
 
   setShortDescription(parser, "kmer_pa_matrix");
   setVersion(parser, "0.0.1");
-  setDate(parser, "January 2024");
+  setDate(parser, "April 2024");
   addUsageLine(parser, "-k kmerdb \
                         [\\fIOPTIONS\\fP] ");
   addDescription(parser, "");
@@ -58,6 +66,8 @@ seqan2::ArgumentParser::ParseResult parseCommandLine(ModifyStringOptions & optio
 
   getOptionValue(options.kmerDatabasesFilenames, parser, "kmer-databases");
   getOptionValue(options.masterKmerDatabase, parser, "master-database");
+  getOptionValue(options.specific_kmer, parser, "specific-kmer");
+  getOptionValue(options.outputFilename, parser, "output-file");
 
   return ArgumentParser::PARSE_OK;
 }
@@ -132,6 +142,10 @@ int checkForPA(CharString currentDBtoCheck, vector<kmer> &v, int n)
 	kmer_query_database.Info(_kmer_query_length, _query_mode, _query_counter_size, _query_lut_prefix_length, _query_sig_len, _query_min_count, _query_max_count, _query_total_kmers);
 	CKmerAPI kmer_object(_kmer_query_length);
 
+	uint64 integer_to_work_on = ((n+1) + 63) / 64;
+	uint64 integer_before = integer_to_work_on-1;
+	uint64 num_bits = n - (integer_before*64);
+
         for(uint64 c = 0; c < length(v); c++)
 	{
 		string k;
@@ -141,7 +155,7 @@ int checkForPA(CharString currentDBtoCheck, vector<kmer> &v, int n)
 		kmer_object.to_string(meh);
 		if(kmer_query_database.IsKmer(kmer_object) == 1)
 		{
-			v[c].bits ^= (static_cast<uint64_t>(1) << n);
+			v[c].bits[integer_to_work_on-1] ^= (static_cast<uint64_t>(1) << num_bits);
 		}
 	}
 
@@ -187,8 +201,19 @@ int createOutputArray(CharString masterKmers, int n, vector<kmer> &v, uint32 &km
 	cerr << "Master database consists of " << _total_kmers << " " << _kmer_length << "-mers"<< endl;
 	v.resize(_total_kmers);
 	uint64 c = 0;
-	boost::dynamic_bitset<> x(n);
-	
+	vector<bool> x(n, false);
+	size_t bytes_used = x.size() / 8 + (x.size() % 8 != 0);
+	cout << "bytes used to represent a kmer: " << bytes_used << endl;
+	size_t totalram = ((bytes_used+8)*_total_kmers) / (1024 * 1024 * 1024);
+	cout << "estimated RAM " << totalram << " GB" << endl;
+
+	vector<uint64> bits;
+	size_t numUint64 = (n + 63) / 64;
+	for(int i = 0; i < numUint64; i++)
+	{
+		uint64 b = 0;
+		bits.push_back(b);
+	}
 
 	while(kmer_database.ReadNextKmer(kmer_object, counter))
 	{
@@ -197,8 +222,7 @@ int createOutputArray(CharString masterKmers, int n, vector<kmer> &v, uint32 &km
 		unsigned long long int encoded;
 		encode(str, encoded);
 		v[c].k = encoded;
-		v[c].bits = 0;
-		v[c].x = x;
+		v[c].bits = bits;
 		c++;
 	}
 
@@ -208,6 +232,56 @@ int createOutputArray(CharString masterKmers, int n, vector<kmer> &v, uint32 &km
 	auto duration = duration_cast<seconds>(stop - start);
 	cerr << "Master kmer database processed: " << duration.count() << " seconds" << endl;
 
+	return 0;
+}
+
+int writeOutPA(vector<kmer> &pa_matrix, CharString outputFilename)
+{
+        // write out
+	std::ofstream outfile(toCString(outputFilename), std::ios::binary);
+	if (!outfile.is_open())
+	{
+		std::cerr << "Failed to open file: " << outputFilename << std::endl;
+		return 1;
+	}
+
+	for(auto i : pa_matrix)
+	{
+		outfile.write(reinterpret_cast<const char*>(&i.k), sizeof(i.k));
+		for (auto bit : i.bits)
+		{
+			outfile.write(reinterpret_cast<const char*>(&bit), sizeof(bit));
+		}
+	}
+	outfile.close();
+
+	return 0;
+}
+
+int readInPA(vector<kmer> &pa_matrix, CharString inputFilename, vector<CharString> kmer_dbs)
+{
+        std::ifstream infile(toCString(inputFilename), std::ios::binary);
+	if (!infile.is_open())
+	{
+		std::cerr << "Failed to open file: " << inputFilename << std::endl;
+		return 1;
+	}
+
+	kmer matrix;
+	size_t numUint64 = (length(kmer_dbs) + 63) / 64;
+
+	while (infile.read(reinterpret_cast<char*>(&matrix.k), sizeof(matrix.k)))
+	{
+		cout << matrix.k << "\t";
+		for (size_t i = 0; i < numUint64; ++i)
+		{
+			uint64 bit;
+			infile.read(reinterpret_cast<char*>(&bit), sizeof(uint64));
+			cout << bit << "\t";
+		}
+		cout << endl;
+	}
+	infile.close();
 	return 0;
 }
 
@@ -234,24 +308,24 @@ int main(int argc, char const ** argv)
 	int counter = 0;
 	for(auto i : kmer_dbs)
 	{
-		cout << i << "\t";
-		cerr << "Processing " << i << " " << (counter+1) << "/"<< length(kmer_dbs) << endl;
-		auto start = high_resolution_clock::now();
-		checkForPA(i, pa_matrix, counter);
-		auto stop = high_resolution_clock::now();
-	        auto duration = duration_cast<seconds>(stop - start);
-        	cerr << "Completed in : " << duration.count() << " seconds" << endl;		
+		if(counter == options.specific_kmer)
+		{
+			cout << i << "\t";
+			cerr << "Processing " << i << " " << (counter+1) << "/"<< length(kmer_dbs) << endl;
+			auto start = high_resolution_clock::now();
+			checkForPA(i, pa_matrix, counter);
+			auto stop = high_resolution_clock::now();
+		        auto duration = duration_cast<seconds>(stop - start);
+	        	cerr << "Completed in : " << duration.count() << " seconds" << endl;
+		}
 		counter++;
 	}
 	cout << endl;
 
-	for(auto i : pa_matrix)
-	{
-		//string k;
-		//decode(i.k, k, kmer_length);
-		//cout << k << "\t";
-		cout << i.k << "\t" << i.bits << endl;;
-	}
+
+	writeOutPA(pa_matrix, options.outputFilename);
+
+	//readInPA(pa_matrix, options.outputFilename, kmer_dbs);
 
 	return 0;
 
