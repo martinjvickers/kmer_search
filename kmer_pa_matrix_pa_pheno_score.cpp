ea6fcc21231736782edc3d455955abda8d3fac27
Martin Vickers <martinj.vickers@gmail.com>
2025-02-21 20:08:52 +0000

    Cleared out old code

--- kmer_pa_matrix_pa_pheno_score.cpp
+++ kmer_pa_matrix_pa_pheno_score.cpp
@@ -85,13 +85,9 @@ seqan2::ArgumentParser::ParseResult parseCommandLine(ModifyStringOptions & optio
 			           "Number of blocks of 1MB to read into memory for each PA matrix you have",
 				   ArgParseArgument::INTEGER, "INT"));
 
-  addOption(parser, ArgParseOption("g", "gene-of-interest",
-                                   "This is a fasta file of your gene.",
-                                   ArgParseArgument::STRING, "TEXT", true));
-
   setShortDescription(parser, "kmer_pa_matrix");
   setVersion(parser, "0.0.1");
-  setDate(parser, "April 2024");
+  setDate(parser, "Feb 2025");
   addUsageLine(parser, "-k kmerdb -i pa_matrix.bin \
                         [\\fIOPTIONS\\fP] ");
   addDescription(parser, "");
@@ -104,7 +100,6 @@ seqan2::ArgumentParser::ParseResult parseCommandLine(ModifyStringOptions & optio
   getOptionValue(options.kmerDatabasesFilenames, parser, "kmer-databases");
   getOptionValue(options.phenotypeFilename, parser, "phenotype-file");
   getOptionValue(options.outputFilename, parser, "input-matrix-file");
-  getOptionValue(options.geneFilename, parser, "gene-of-interest");
   getOptionValue(options.kmer_size, parser, "kmer-size");
   getOptionValue(options.threads, parser, "threads");
   getOptionValue(options.blocks, parser, "blocks");
@@ -196,205 +191,6 @@ int decode(const unsigned long long int& x_ull, std::string& kmer, uint32 kmer_l
 	return 0;
 }
 
-// Function to delete the lock file
-void deleteLockFile(CharString outputFilename)
-{
-	if (remove(toCString(outputFilename)) != 0)
-	{
-		std::cerr << "Error deleting lock file." << std::endl;
-	}
-	else
-	{
-		std::cout << "Lock file deleted successfully." << std::endl;
-	}
-}
-
-bool createLockFile(CharString outputFilename)
-{
-	int fd = open(toCString(outputFilename), O_CREAT | O_EXCL | O_WRONLY, 0666);
-	if (fd == -1)
-	{
-		return false; // Failed to create lock file, likely because it already exists
-	}
-
-	char hostname[256];
-	gethostname(hostname, sizeof(hostname));
-	pid_t pid = getpid();
-
-	std::string content = "Hostname: " + std::string(hostname) + "\nProcess ID: " + std::to_string(pid) + "\n";
-	ssize_t bytesWritten = ::write(fd, content.c_str(), content.size());  // Using ::write to specify from unistd.h
-
-	if(bytesWritten == -1)
-	{
-		cout << "Error writing to file" << endl;
-		close(fd);
-		return false;
-	}
-
-	if(fsync(fd) == 1)
-	{
-		cout << "Error syncing to disk" << endl;
-		close(fd);
-		return false;
-	}
-
-	close(fd);
-	return true;
-}
-
-int getListOfKmers(CharString masterDatabase, vector<uint64> &v)
-{
-	KMC::Runner runner;
-	CKMCFile kmer_database;
-	if(!kmer_database.OpenForListing(toCString(masterDatabase)))
-	{
-		cout <<"Error opening kmer database file." << endl;
-		return 1;
-	}
-	// read existing kmer database, which gives us our kmer length
-	uint32 _kmer_length, _mode, _counter_size, _lut_prefix_length, _sig_len, _min_count;
-	uint64 _max_count;
-	uint64 _total_kmers;
-	kmer_database.Info(_kmer_length, _mode, _counter_size, _lut_prefix_length, _sig_len, _min_count, _max_count, _total_kmers);
-	CKmerAPI kmer_object(_kmer_length);
-	uint32 counter;
-	uint64 c;
-
-	v.resize(_total_kmers);
-
-	while(kmer_database.ReadNextKmer(kmer_object, counter))
-	{
-		unsigned long long int encoded;
-		string str;
-		kmer_object.to_string(str);
-		encode(str, encoded);
-		v[c] = encoded;
-	}
-	
-	return 0;
-}
-
-int checkForPA(CharString masterKmers, CharString currentKmers, CharString inputFilename, vector<CharString> kmer_dbs, int position_to_edit)
-{
-	// this vector contains the true/false of each of our master kmers found in the current kmer list
-	vector<bool> v;
-        KMC::Runner runner;
-	CKMCFile kmer_database;
-	if(!kmer_database.OpenForListing(toCString(masterKmers)))
-	{
-		cout <<"Error opening kmer database file." << endl;
-		return 1;
-	}
-	// read existing kmer database, which gives us our kmer length
-	uint32 _kmer_length, _mode, _counter_size, _lut_prefix_length, _sig_len, _min_count;
-	uint64 _max_count;
-	uint64 _total_kmers;
-	kmer_database.Info(_kmer_length, _mode, _counter_size, _lut_prefix_length, _sig_len, _min_count, _max_count, _total_kmers);
-	CKmerAPI kmer_object(_kmer_length);
-
-        // this is the query database for random access searching
-	CKMCFile kmer_query_database;
-	if(!kmer_query_database.OpenForRA(toCString(currentKmers)))
-	{
-		cout <<"Error opening kmer database file " << currentKmers << endl;
-		return 1;
-	}
-	uint32 _kmer_query_length, _query_mode, _query_counter_size, _query_lut_prefix_length, _query_sig_len, _query_min_count;
-	uint64 _query_max_count, _query_total_kmers;
-	kmer_query_database.Info(_kmer_query_length, _query_mode, _query_counter_size, _query_lut_prefix_length, _query_sig_len, _query_min_count, _query_max_count, _query_total_kmers);
-	CKmerAPI kmer_query_object(_kmer_query_length);
-	uint32 counter;
-
-	// for each master kmer, we append our kmers to the vector
-	while(kmer_database.ReadNextKmer(kmer_object, counter))
-	{
-		unsigned long long int encoded;
-		string str;
-		kmer_object.to_string(str);
-		encode(str, encoded);
-		if(kmer_query_database.IsKmer(kmer_object) == 1)
-		{
-			v.push_back(1);
-		}
-		else
-		{
-			v.push_back(0);
-		}
-	}
-
-	kmer_query_database.Close();
-	kmer_database.Close();
-
-	// read in file and edit it
-	std::fstream infile(toCString(inputFilename), std::ios::in | std::ios::out | std::ios::binary);
-        //if (!infile.is_open())
-	if (!infile)
-        {
-                std::cerr << "Failed to open file: " << inputFilename << std::endl;
-                return 1;
-        }		
-
-	//this calculates our vector which contains the number of bytes we need to cover our number of accessions
-	//this might not be needed. I will give it some thought
-	size_t numUint64 = (length(kmer_dbs) + 63) / 64;
-	vector<uint64> bits;
-	for(int i = 0; i < numUint64; i++)
-	{
-		uint64 b = 0;
-		bits.push_back(b);
-	}
-	int byte_to_edit = (((position_to_edit+1) + 63) / 64)-1;
-	int bit_to_edit = position_to_edit - (byte_to_edit*64);
-
-	// remember that the first byte contains the encoded kmer
-	std::streampos position = (0+8) + (byte_to_edit * 8);
-	uint64 count = 0; // the row we are editing
-
-	CharString lockExtension = ".lock.";
-	CharString lockFilename = inputFilename;
-	CharString byte_suffix = to_string(byte_to_edit);
-	append(lockFilename, lockExtension);
-	append(lockFilename, byte_suffix);
-	cout << "Lockfile " << lockFilename << endl;
-
-	while(true)
-	{
-		if(createLockFile(lockFilename))
-		{
-			break;
-		}
-		else
-		{
-			// i guess this could be a random number
-			cout << "File locked, waiting 60 seconds" << endl;
-			sleep(60);
-		}
-	}
-
-	while(count < length(v))
-	{
-		if(v[count] == 1)
-		{
-			uint64 byte;
-			infile.seekg(position);
-                	infile.read(reinterpret_cast<char*>(&byte), sizeof(uint64));
-		        infile.seekp(position);
-			byte = (((uint64_t)1) << bit_to_edit) | byte;
-			infile.write(reinterpret_cast<char*>(&byte), sizeof(byte));
-		}
-
-		//position = position + ((numUint64+1)*8);
-		position += static_cast<std::streamoff>((numUint64 + 1) * 8);
-		count++;
-	}
-
-	infile.close();
-
-	deleteLockFile(lockFilename);
-
-	return 0;
-}
-
 vector<CharString> createFileList(CharString kmerDatabaseList)
 {
 	vector<CharString> v;
@@ -472,69 +268,6 @@ void createKmerFileList(CharString kmers_to_search_for, std::set<uint64> &v)
 	}
 }
 
-int createOutputArray(CharString masterKmers, int n, vector<kmer> &v, uint32 &kmer_length, CharString outputFilename)
-{
-        // write out
-	std::ofstream outfile(toCString(outputFilename), std::ios::binary);
-	if (!outfile.is_open())
-	{
-		std::cerr << "Failed to open file: " << outputFilename << std::endl;
-		return 1;
-	}
-	
-	auto start = high_resolution_clock::now();
-	KMC::Runner runner;
-	CKMCFile kmer_database;
-	if(!kmer_database.OpenForListing(toCString(masterKmers)))
-	{
-		cout <<"Error opening kmer database file." << endl;
-		return 1;
-	}
-	// read existing kmer database, which gives us our kmer length
-	uint32 _kmer_length, _mode, _counter_size, _lut_prefix_length, _sig_len, _min_count;
-	uint64 _max_count;
-	uint64 _total_kmers;
-	kmer_database.Info(_kmer_length, _mode, _counter_size, _lut_prefix_length, _sig_len, _min_count, _max_count, _total_kmers);
-	kmer_length = _kmer_length;
-	CKmerAPI kmer_object(_kmer_length);
-	uint32 counter;
-
-	v.resize(_total_kmers);
-	uint64 c; // counter
-
-	vector<uint64> bits;
-	size_t numUint64 = (n + 63) / 64;
-	for(int i = 0; i < numUint64; i++)
-	{
-		uint64 b = 0;
-		bits.push_back(b);
-
-	}
-
-	while(kmer_database.ReadNextKmer(kmer_object, counter))
-	{
-		string str;
-		kmer_object.to_string(str);
-		unsigned long long int encoded;
-		encode(str, encoded);
-		outfile.write(reinterpret_cast<const char*>(&encoded), sizeof(encoded));
-		for (auto bit : bits)
-		{
-			outfile.write(reinterpret_cast<const char*>(&bit), sizeof(bit));
-		}
-		c++;
-	}
-
-	kmer_database.Close();
-	outfile.close();
-
-	auto stop = high_resolution_clock::now();
-	auto duration = duration_cast<seconds>(stop - start);
-	cerr << "Master kmer database processed: " << duration.count() << " seconds" << endl;
-
-	return 0;
-}
-
 int writeOutPA(vector<kmer> &pa_matrix, CharString outputFilename)
 {
         // write out
@@ -564,92 +297,6 @@ uint64_t reverseBits(uint64_t n)
 	string s = bits.to_string();
 	std::reverse(s.begin(), s.end());
 	return bitset<64>(s).to_ulong();
-}
-
-int readInPA(vector<kmer> &pa_matrix, CharString inputFilename, vector<CharString> kmer_dbs, std::set<uint64> &to_find, int kmer_size)
-{
-        std::ifstream infile(toCString(inputFilename), std::ios::binary);
-	if (!infile.is_open())
-	{
-		std::cerr << "Failed to open file: " << inputFilename << std::endl;
-		return 1;
-	}
-
-	kmer matrix;
-	size_t numUint64 = (length(kmer_dbs) + 63) / 64;
-	int bits_at_the_end = 64 - ((numUint64 * 64) - length(kmer_dbs));
-
-	// read a kmer
-	while (infile.read(reinterpret_cast<char*>(&matrix.k), sizeof(matrix.k)))
-	{
-		string kmer;
-		decode(matrix.k, kmer, kmer_size); 
-
-		if(to_find.find(matrix.k) != to_find.end())
-		{
-			cout << kmer << "\t" << matrix.k << "\t";
-		}
-
-		for (size_t i = 0; i < numUint64; ++i)
-		{
-			uint64 byte;
-			infile.read(reinterpret_cast<char*>(&byte), sizeof(uint64));
-			uint64 rev = reverseBits(byte);
-			std::bitset<64> x(rev);
-			if(to_find.find(matrix.k) != to_find.end())
-	                {
-				if(i < numUint64-1)
-				{
-					cout << x;
-				}
-				else if(i == numUint64-1)
-				{
-					for(int j = 0; j < bits_at_the_end; j++)
-					{
-						std::bitset<64> t(byte);
-						cout << t[j];
-					}
-				}
-			}
-		}
-
-		if(to_find.find(matrix.k) != to_find.end())
-		{
-			cout << endl;
-		}
-
-	}
-	infile.close();
-	return 0;
-}
-
-void kmers_from_gene(CharString filename, std::set<uint64> &kmers_to_search_for, int kmer_size)
-{
-	CharString id;
-	Dna5String seq;
-	SeqFileIn seqFileIn(toCString(filename));
-	readRecord(id, seq, seqFileIn);
-
-	for(int i = 0; i < length(seq)-kmer_size-1; i++)
-	{
-		Dna5String inf = infix(seq, i, i+kmer_size);
-		unsigned long long int encoded;
-                encode(inf, encoded);
-		kmers_to_search_for.insert(encoded);
-		reverseComplement(inf);
-		encode(inf, encoded);
-		kmers_to_search_for.insert(encoded);
-	}
-	close(seqFileIn);
-/*
-	cout << "Kmers to search for" << endl;
-	for(auto i : kmers_to_search_for)
-	{
-		string kmer;
-		decode(i, kmer, kmer_size);
-		cout << kmer << "\t" << i << endl;
-	}
-*/
 }
 
 int openAllFiles(vector<ifstream> &fileStreams, int numFiles, vector<CharString> &filenames)
@@ -726,20 +373,22 @@ int readLineFromAllFilesBuf(vector<ifstream> &fileStreams, int numFiles, vector<
         return 0;
 }
 
-struct ThreadResult {
-    kmer k_value;
-    vector<double> vec;
+struct ThreadResult
+{
+	kmer k_value;
+	vector<double> vec;
 };
 
-struct ThreadData {
-    vector<kmer> *matrix_buf;
-    vector<CharString> *accessionNamesInPA;
-    map<CharString, vector<double>> *phenotypes;
-    vector<int> *pheno_to_accession_map;
-    int num_phenoms;
-    size_t start_idx, end_idx;
-    vector<ThreadResult> *results; // Store results
-    pthread_mutex_t *mutex; // Synchronization
+struct ThreadData
+{
+	vector<kmer> *matrix_buf;
+	vector<CharString> *accessionNamesInPA;
+	map<CharString, vector<double>> *phenotypes;
+	vector<int> *pheno_to_accession_map;
+	int num_phenoms;
+	size_t start_idx, end_idx;
+	vector<ThreadResult> *results; // Store results
+	pthread_mutex_t *mutex; // Synchronization
 };
 
 void *process_chunk(void *arg) {
@@ -788,8 +437,6 @@ void *process_chunk(void *arg) {
     pthread_exit(nullptr);
 }
 
-
-
 int process_phenotype_scores(vector<kmer> &matrix_buf, vector<CharString> &accessionNamesInPA,
 		             map<CharString, vector<double>> &phenotypes, int num_phenoms,
 			     vector<int> &pheno_to_accession_map, int threads, vector<ThreadResult> &results)
@@ -803,7 +450,8 @@ int process_phenotype_scores(vector<kmer> &matrix_buf, vector<CharString> &acces
 
     size_t chunk_size = matrix_buf.size() / NUM_THREADS;
 
-    for (int t = 0; t < NUM_THREADS; ++t) {
+    for (int t = 0; t < NUM_THREADS; ++t)
+    {
         thread_data[t] = {
             &matrix_buf, &accessionNamesInPA, &phenotypes, &pheno_to_accession_map,
             num_phenoms, t * chunk_size, (t == NUM_THREADS - 1) ? matrix_buf.size() : (t + 1) * chunk_size,
@@ -844,8 +492,6 @@ int process_phenotype_scores(vector<kmer> &matrix_buf, vector<CharString> &acces
 		// loop through each accession with a phenotype
 		for(auto i : pheno_to_accession_map)
 		{
-			//cout << accessionNamesInPA[i] << " exists" << endl;
-			//auto &phenotype_vec = phenotypes[accessionNamesInPA[i]];
 			CharString &accession = accessionNamesInPA[i];
 			auto &phenotype_vec = phenotypes[accession];
 
@@ -857,23 +503,11 @@ int process_phenotype_scores(vector<kmer> &matrix_buf, vector<CharString> &acces
 				num_bits_set++;
 				for(int p = 0; p < num_phenoms; p++)
 				{
-					//vec[p] = vec[p] + (1 * phenotype_vec[p]);
 					vec[p] += phenotype_vec[p];
 
 				}
 			}
 		}
-		/*
-		string kmer;
-		decode(k.k, kmer, 31);
-		cout << kmer << "\t";
-		for(auto s : vec)
-		{
-			cout << s << "\t";
-			cout << s/num_bits_set << "\t";
-		}
-		cout << num_bits_set << "\t";
-		cout << endl;*/
 	}
 
 	return 0;
@@ -881,17 +515,18 @@ int process_phenotype_scores(vector<kmer> &matrix_buf, vector<CharString> &acces
 
 int printAresult(vector<ThreadResult> &results)
 {
-	//cout << results.back().k_value.k << "\t";
-	string kmer;
-	decode(results.back().k_value.k, kmer, 31);
-
-	cout << kmer << "\t";
-	for(auto v : results.back().vec)
+	for(auto r : results)
 	{
-		cout << v << "\t";
-	}
-	cout << endl;
+		string kmer;
+		decode(results.back().k_value.k, kmer, 31);
 
+		cout << kmer << "\t";
+		for(auto v : results.back().vec)
+		{
+			cout << v << "\t";
+		}
+		cout << endl;
+	}
 	return 0;
 }
 
@@ -999,18 +634,10 @@ int main(int argc, char const ** argv)
 	// what we need is a vector of mappings from the phenotype name to the location in kmer_dbs
 	vector<int> pheno_to_accession_map = createPheno2AM(accession_names, phenotypes);
 
-	/*
-	for(auto i : phenotypeNames)
-		cout << i << endl;
-	for(auto i : phenotypes)
-		cout << i.first << "\t" << i.second[0] << endl;
-	*/
-
 	// get list of matrix files
 	vector<CharString> matrixFilenames = createFileList(options.outputFilename);
 
 	process(matrixFilenames, accession_names, phenotypes, phenotypeNames, pheno_to_accession_map);
 
 	return 0;
-
 }
